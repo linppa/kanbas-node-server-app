cfc1038272116e3579a9a8958c5873936018e629
linda <linnppa@gmail.com>
2024-04-04 01:25:46 -0400

    update courses

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
+import CourseRoutes from './Kanbas/courses/routes.js';
 import cors from 'cors';
 
 const app = express()
 app.use(cors());
 app.use(express.json());
 
-Lab5(app)
-Hello(app)
-app.listen(4000)
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,274 @@
+export default [
+  {
+    "_id": "M101",
+    "name": "Introduction to Rocket Propulsion",
+    "description": "Basic principles of rocket propulsion and rocket engines.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "History of Rocketry",
+        "description": "A brief history of rocketry and space exploration.",
+        "module": "M101"
+      },
+      {
+        "_id": "L102",
+        "name": "Rocket Propulsion Fundamentals",
+        "description": "Basic principles of rocket propulsion.",
+        "module": "M101"
+      },
+      {
+        "_id": "L103",
+        "name": "Rocket Engine Types",
+        "description": "Overview of different types of rocket engines.",
+        "module": "M101"
+      }
+    ]
+  },
+  {
+    "_id": "M102",
+    "name": "Fuel and Combustion",
+    "description": "Understanding rocket fuel, combustion processes, and efficiency.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "_id": "L201",
+        "name": "Rocket Fuel",
+        "description": "Overview of different types of rocket fuels.",
+        "module": "M102"
+      },
+      {
+        "_id": "L202",
+        "name": "Combustion Processes",
+        "description": "Understanding combustion processes and efficiency.",
+        "module": "M102"
+      },
+      {
+        "_id": "L203",
+        "name": "Combustion Instability",
+        "description": "Understanding combustion instability and mitigation.",
+        "module": "M102"
+      }
+    ]
+  },
+  {
+    "_id": "M103",
+    "name": "Nozzle Design",
+    "description": "Principles of rocket nozzle design and performance optimization.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "_id": "L301",
+        "name": "Nozzle Design",
+        "description": "Overview of different types of rocket nozzles.",
+        "module": "M103"
+      },
+      {
+        "_id": "L302",
+        "name": "Nozzle Performance",
+        "description": "Understanding nozzle performance and efficiency.",
+        "module": "M103"
+      },
+      {
+        "_id": "L303",
+        "name": "Nozzle Optimization",
+        "description": "Optimizing nozzle design for specific applications.",
+        "module": "M103"
+      }
+    ]
+  },
+  {
+    "_id": "M201",
+    "name": "Fundamentals of Aerodynamics",
+    "description": "Basic aerodynamic concepts and fluid dynamics principles.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M202",
+    "name": "Subsonic and Supersonic Flow",
+    "description": "Understanding subsonic and supersonic aerodynamic behaviors.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M203",
+    "name": "Aerodynamic Heating",
+    "description": "Study of aerodynamic heating and thermal protection systems.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M301",
+    "name": "Spacecraft Structural Design",
+    "description": "Fundamentals of designing spacecraft structures and materials selection.",
+    "course": "RS103"
+  },
+  {
+    "_id": "M302",
+    "name": "Orbital Mechanics",
+    "description": "Understanding orbital dynamics and mission planning.",
+    "course": "RS103"
+  },
+  {
+    "_id": "M303",
+    "name": "Spacecraft Systems Engineering",
+    "description": "Overview of spacecraft systems and subsystems engineering.",
+    "course": "RS103"
+  },
+
+  {
+    "_id": "M501",
+    "name": "Introduction to Web Development",
+    "description": "Foundational concepts of web development, including HTML, CSS, and JavaScript basics.",
+    "course": "CS5610",
+    "lessons": [
+      {
+        "_id": "L501",
+        "name": "Basics of HTML",
+        "description": "Introduction to HTML elements, structure, and semantics.",
+        "module": "M501"
+      },
+      {
+        "_id": "L502",
+        "name": "Styling with CSS",
+        "description": "Fundamentals of CSS for website styling.",
+        "module": "M501"
+      },
+      {
+        "_id": "L503",
+        "name": "Introduction to JavaScript",
+        "description": "Basic programming concepts in JavaScript for dynamic content.",
+        "module": "M501"
+      }
+    ]
+  },
+  {
+    "_id": "M502",
+    "name": "Advanced Web Development",
+    "description": "Advanced topics in web development, including frameworks, SPA, and server-side programming.",
+    "course": "CS5610",
+    "lessons": [
+      {
+        "_id": "L504",
+        "name": "Frameworks and Libraries",
+        "description": "Overview of popular JavaScript frameworks and libraries.",
+        "module": "M502"
+      },
+      {
+        "_id": "L505",
+        "name": "Single Page Applications (SPA)",
+        "description": "Design and development of SPA using modern frameworks.",
+        "module": "M502"
+      },
+      {
+        "_id": "L506",
+        "name": "Server-side Programming",
+        "description": "Introduction to server-side programming and databases.",
+        "module": "M502"
+      }
+    ]
+  },
+  {
+    "_id": "M601",
+    "name": "Fundamentals of Database Management",
+    "description": "Core principles of database management systems and SQL.",
+    "course": "CS5200",
+    "lessons": [
+      {
+        "_id": "L601",
+        "name": "Introduction to Databases",
+        "description": "Basics of database systems and data models.",
+        "module": "M601"
+      },
+      {
+        "_id": "L602",
+        "name": "SQL and Database Design",
+        "description": "Fundamentals of SQL queries and relational database design.",
+        "module": "M601"
+      },
+      {
+        "_id": "L603",
+        "name": "Normalization",
+        "description": "Understanding normalization and database schemas.",
+        "module": "M601"
+      }
+    ]
+  },
+  {
+    "_id": "M602",
+    "name": "NoSQL and Big Data",
+    "description": "Exploration of NoSQL databases and big data technologies.",
+    "course": "CS5200",
+    "lessons": [
+      {
+        "_id": "L604",
+        "name": "Introduction to NoSQL",
+        "description": "Overview of NoSQL databases and use cases.",
+        "module": "M602"
+      },
+      {
+        "_id": "L605",
+        "name": "Big Data Technologies",
+        "description": "Introduction to big data concepts and the Hadoop ecosystem.",
+        "module": "M602"
+      },
+      {
+        "_id": "L606",
+        "name": "Data Warehousing",
+        "description": "Concepts of data warehousing and business intelligence.",
+        "module": "M602"
+      }
+    ]
+  },
+  {
+    "_id": "M701",
+    "name": "Introduction to Algorithms",
+    "description": "Basic algorithmic techniques and complexity analysis.",
+    "course": "CS5800",
+    "lessons": [
+      {
+        "_id": "L701",
+        "name": "Algorithmic Thinking",
+        "description": "Introduction to the concept of algorithmic thinking.",
+        "module": "M701"
+      },
+      {
+        "_id": "L702",
+        "name": "Complexity Analysis",
+        "description": "Understanding time and space complexity.",
+        "module": "M701"
+      },
+      {
+        "_id": "L703",
+        "name": "Basic Algorithms",
+        "description": "Overview of sorting and searching algorithms.",
+        "module": "M701"
+      }
+    ]
+  },
+  {
+    "_id": "M702",
+    "name": "Data Structures and Advanced Algorithms",
+    "description": "In-depth study of data structures and advanced algorithmic strategies.",
+    "course": "CS5800",
+    "lessons": [
+      {
+        "_id": "L704",
+        "name": "Advanced Data Structures",
+        "description": "Study of trees, graphs, and data structure optimization.",
+        "module": "M702"
+      },
+      {
+        "_id": "L705",
+        "name": "Dynamic Programming",
+        "description": "Techniques and applications of dynamic programming.",
+        "module": "M702"
+      },
+      {
+        "_id": "L706",
+        "name": "Graph Algorithms",
+        "description": "Exploration of algorithms for graph theory problems.",
+        "module": "M702"
+      }
+    ]
+  }
+
+]

e93c12dae54bd129f2007de109a3b95f36d79d36
linda <linnppa@gmail.com>
2024-04-04 18:03:20 -0400

    Update routes.js

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -12,16 +12,9 @@ export default function CourseRoutes(app) {
         res.status(404).send(`Course with id ${id} not found`);
       }
     };
-    app.delete("/api/courses/:id", deleteCourse);
-
-    // app.delete("/api/courses/:id", (req, res) => {
-    //     const { id } = req.params;
-    //     Database.courses = Database.courses
-    //       .filter((c) => c._id !== id);
-    //     res.sendStatus(204);
-    //   });    
+    app.delete("/api/courses/:id", deleteCourse);  
     
-
+    // update course by id
     const updateCourse = (req, res) => {
       const id = req.params.id;
       const index = Database.courses.findIndex((course) => course._id === id);
@@ -33,7 +26,6 @@ export default function CourseRoutes(app) {
       }
     };
     app.put("/api/courses/:id", updateCourse);
-
 
     // create new course & add to database
     app.post("/api/courses", (req, res) => {
@@ -62,69 +54,4 @@ export default function CourseRoutes(app) {
         }
         res.send(course);
       });
-    
-
-//     const findCourseById = (req, res) => {
-//       const id = req.params.id;
-//       const course = db.courses.find((course) => course._id === id);
-//       if (course) {
-//         res.json(course);
-//       } else {
-//         res.status(404).send(`Course with id ${id} not found`);
-//       }
-//     };
-//     app.get("/api/courses/:id", findCourseById);
-
 }
-
-// import * as db from "../Database/index.js";
-// export default function CoursesRoutes(app) {
-//     // CRUD
-//     const fetchAllCourses = (req, res) => {
-//       res.json(db.courses);
-//     };
-
-//     const findCourseById = (req, res) => {
-//       const id = req.params.id;
-//       const course = db.courses.find((course) => course._id === id);
-//       if (course) {
-//         res.json(course);
-//       } else {
-//         res.status(404).send(`Course with id ${id} not found`);
-//       }
-//     };
-
-//     const createCourse = (req, res) => {
-//       const course = { ...req.body, _id: Date.now().toString() };
-//       db.courses.push(course);
-//       res.json(db.courses);
-//     };
-
-//     const updateCourse = (req, res) => {
-//       const id = req.params.id;
-//       const index = db.courses.findIndex((course) => course._id === id);
-//       if (index !== -1) {
-//         db.courses[index] = { ...db.courses[index], ...req.body };
-//         res.json(db.courses);
-//       } else {
-//         res.status(404).send(`Course with id ${id} not found`);
-//       }
-//     };
-
-//     const deleteCourse = (req, res) => {
-//       const id = req.params.id;
-//       const index = db.courses.findIndex((course) => course._id === id);
-//       if (index !== -1) {
-//         db.courses.splice(index, 1);
-//         res.json(db.courses);
-//       } else {
-//         res.status(404).send(`Course with id ${id} not found`);
-//       }
-//     };
-  
-//     app.get("/api/courses", fetchAllCourses);
-//     app.get("/api/courses/:id", findCourseById);
-//     app.post("/api/courses", createCourse);
-//     app.put("/api/courses/:id", updateCourse);
-//     app.delete("/api/courses/:id", deleteCourse);
-//   }
